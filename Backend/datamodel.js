7ca1d4a22d8f434e4a249c1fe34f27b6204d5432
iamsajith <sajithj03@gmail.com>
2022-07-19 19:53:42 +0530

    changes
    ->qr code generator
    ->database connection- student

--- Backend/datamodel.js
+++ Backend/datamodel.js
@@ -1,38 +1,47 @@
 const mongoose = require("mongoose")
 const DB = process.env.MONGODB_URI
-mongoose.connect(DB,{ useNewUrlParser: true, useUnifiedTopology: true}).then(()=>{
+mongoose.connect(DB, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
  console.log("Database Connection Successful")
-}).catch((err)=>{
+}).catch((err) => {
  console.log(err)
 })
 const schema = mongoose.Schema
 
 const studentSchema = new schema({
- email : String,
- password : String,
- pin:Number,
+ email: String,
+ password: String,
+ pin: Number,
+ name: String,
+ email: String,
+ phone: String,
+ course:String,
+ batch: String,
+ image: String,
+ qrcode:String,
+ startDate: String,
+ endDate: String
 
-},{
+}, {
  versionKey: false
 })
 
 const moderatorSchema = new schema({
- email : String,
- password : String,
- pin:Number,
-},{
+ email: String,
+ password: String,
+ pin: Number,
+}, {
  versionKey: false
 })
 
 const adminSchema = new schema({
- email : String,
- password : String,
- pin:Number,
-},{
+ email: String,
+ password: String,
+ pin: Number,
+}, {
  versionKey: false
 })
-const studentData =new mongoose.model('student',studentSchema,'students')
-const moderatorData =new mongoose.model('moderators',moderatorSchema)
-const adminData =new mongoose.model('admins',adminSchema)
+const studentData = new mongoose.model('student', studentSchema, 'students')
+const moderatorData = new mongoose.model('moderators', moderatorSchema)
+const adminData = new mongoose.model('admins', adminSchema)
 
-module.exports = {studentData,moderatorData,adminData}
+module.exports = { studentData, moderatorData, adminData }
--- Backend/index.js
+++ Backend/index.js
@@ -3,10 +3,12 @@ const mongoose = require("mongoose")
 const cors = require("cors")
 const dotenv = require("dotenv").config()
 const path = require('path');
-const {studentData,moderatorData,adminData} = require("./datamodel")
+const { studentData, moderatorData, adminData } = require("./datamodel")
 const jwt = require("jsonwebtoken")
 const nodemailer = require("nodemailer")
-const {google} = require("googleapis")
+const { google } = require("googleapis")
+const QRCode = require('qrcode');
+const { datacatalog } = require("googleapis/build/src/apis/datacatalog");
 
 
 
@@ -14,37 +16,47 @@ const app = new express();
 app.use(cors());
 app.use(express.json({ urlencoded: true }));
 
+
+// Require the package
+
+
+// QR Code
+
+
+
+
+
 // Send Mail 
 
-  const CLIENT_ID = process.env.CLIENT_ID
-  const CLIENT_SECRET = process.env.CLIENT_SECRET
-  const REDIRECT_URI = process.env.REDIRECT_URI
-  const REFRESH_TOKEN = process.env.REFRESH_TOKEN
-  const oAuth2Client = new google.auth.OAuth2(CLIENT_ID,CLIENT_SECRET,REDIRECT_URI,REFRESH_TOKEN)
-  oAuth2Client.setCredentials({refresh_token:REFRESH_TOKEN})
+const CLIENT_ID = process.env.CLIENT_ID
+const CLIENT_SECRET = process.env.CLIENT_SECRET
+const REDIRECT_URI = process.env.REDIRECT_URI
+const REFRESH_TOKEN = process.env.REFRESH_TOKEN
+const oAuth2Client = new google.auth.OAuth2(CLIENT_ID, CLIENT_SECRET, REDIRECT_URI, REFRESH_TOKEN)
+oAuth2Client.setCredentials({ refresh_token: REFRESH_TOKEN })
 
-  const sendMail = async(step,email,password)=>{
-    if (step === 0){
-   
+const sendMail = async (step, email, password) => {
+  if (step === 0) {
+
     try {
 
       const accessToken = await oAuth2Client.getAccessToken
       const transport = nodemailer.createTransport({
-        service:"gmail",
-        auth:{
-          type:"OAuth2",
-          user:process.env.EMAIL,
-          clientId : CLIENT_ID,
-          clientSecret :CLIENT_SECRET,
-          refreshToken:REFRESH_TOKEN,
-          accessToken:accessToken
+        service: "gmail",
+        auth: {
+          type: "OAuth2",
+          user: process.env.EMAIL,
+          clientId: CLIENT_ID,
+          clientSecret: CLIENT_SECRET,
+          refreshToken: REFRESH_TOKEN,
+          accessToken: accessToken
         }
       })
       const mailOptions = {
-        from:'ID Generator <process.env.EMAIL>',
-        to:email,
-        subject:"Reset Password",
-        html:`<em>Hi👋, You have requested to reset your password. We can not simply send you your new password. You can use the PIN which is attached below to change your password.<em style='text-align: center'><h2>${password}</h2>`
+        from: 'ID Generator <process.env.EMAIL>',
+        to: email,
+        subject: "Reset Password",
+        html: `<em>Hi👋, You have requested to reset your password. We can not simply send you your new password. You can use the PIN which is attached below to change your password.<em style='text-align: center'><h2>${password}</h2>`
       }
       const result = await transport.sendMail(mailOptions)
       return result
@@ -52,26 +64,26 @@ app.use(express.json({ urlencoded: true }));
       return error
     }
   }
-  else{
+  else {
     try {
 
       const accessToken = await oAuth2Client.getAccessToken
       const transport = nodemailer.createTransport({
-        service:"gmail",
-        auth:{
-          type:"OAuth2",
-          user:process.env.EMAIL,
-          clientId : CLIENT_ID,
-          clientSecret :CLIENT_SECRET,
-          refreshToken:REFRESH_TOKEN,
-          accessToken:accessToken
+        service: "gmail",
+        auth: {
+          type: "OAuth2",
+          user: process.env.EMAIL,
+          clientId: CLIENT_ID,
+          clientSecret: CLIENT_SECRET,
+          refreshToken: REFRESH_TOKEN,
+          accessToken: accessToken
         }
       })
       const mailOptions = {
-        from:'ID Generator <process.env.EMAIL>',
-        to:email,
-        subject:"Updated Password",
-        html:`<em>Hi👋, Your password has been successfully updated. Please use the below as your new password.<em style='text-align: center'><h2>${password}</h2>`
+        from: 'ID Generator <process.env.EMAIL>',
+        to: email,
+        subject: "Updated Password",
+        html: `<em>Hi👋, Your password has been successfully updated. Please use the below as your new password.<em style='text-align: center'><h2>${password}</h2>`
       }
       const result = await transport.sendMail(mailOptions)
       return result
@@ -79,7 +91,7 @@ app.use(express.json({ urlencoded: true }));
       return error
     }
   }
-  }
+}
 
 // #1 Student - Authentication
 
@@ -97,7 +109,7 @@ app.post("/student", (req, res) => {
     if (user !== null) {
       let payload = { subject: user.email + user.password };
       let token = jwt.sign(payload, "secretKey");
-      res.status(200).send({token: token, data:user._id });
+      res.status(200).send({ token: token, data: user._id });
     }
     else {
       res.status(401).send('Wrong Credentials')
@@ -121,7 +133,7 @@ app.post("/moderator", (req, res) => {
     if (user !== null) {
       let payload = { subject: user.email + user.password };
       let token = jwt.sign(payload, "secretKey");
-      res.status(200).send({ token, data:user._id });
+      res.status(200).send({ token, data: user._id });
     }
     else {
       res.status(401).send('Wrong Credentials')
@@ -145,7 +157,7 @@ app.post("/admin", (req, res) => {
     if (user !== null) {
       let payload = { subject: user.email + user.password };
       let token = jwt.sign(payload, "secretKey");
-      res.status(200).send({ token, data:user._id });
+      res.status(200).send({ token, data: user._id });
     }
     else {
       res.status(401).send('Wrong Credentials')
@@ -155,36 +167,36 @@ app.post("/admin", (req, res) => {
 
 // Student PIN
 
-app.post("/student/pin",(req,res)=>{
-  
+app.post("/student/pin", (req, res) => {
+
   const randomPin = Math.floor(1000 + Math.random() * 9000)
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Method:GET,POST,PUT,DELETE");
-  studentData.findOneAndUpdate({email:req.body.email},{$set:{pin:randomPin}}).then(()=>{
+  studentData.findOneAndUpdate({ email: req.body.email }, { $set: { pin: randomPin } }).then(() => {
     const mail = req.body.email
-    sendMail(step=0,mail,randomPin).then((result)=>console.log("Send Successfully",result))
-    .catch((error)=>{console.log(error)})
+    sendMail(step = 0, mail, randomPin).then((result) => console.log("Send Successfully", result))
+      .catch((error) => { console.log(error) })
     res.send()
   })
 
 })
 // Student New Password
 
-app.post("/student/newpassword",(req,res)=>{
-  studentnew = (Math.random().toString(36).substring(2, 5)+"stu"+Math.random().toString(36).substring(2, 6))
+app.post("/student/newpassword", (req, res) => {
+  studentnew = (Math.random().toString(36).substring(2, 5) + "stu" + Math.random().toString(36).substring(2, 6))
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Method:GET,POST,PUT,DELETE");
   pin = req.body.pin
-  studentData.findOneAndUpdate({email:req.body.email,pin:pin},{$set:{password:studentnew}}).then((data)=>{
-    if(data !== null){
+  studentData.findOneAndUpdate({ email: req.body.email, pin: pin }, { $set: { password: studentnew } }).then((data) => {
+    if (data !== null) {
       const mail = req.body.email
-      sendMail(step=1,mail,studentnew).then((result)=>console.log(result))
-      .catch((error)=>{console.log(error)})
+      sendMail(step = 1, mail, studentnew).then((result) => console.log(result))
+        .catch((error) => { console.log(error) })
       res.send()
-      }
-      else{
-        res.status(404).send("Data not found")
-      }
+    }
+    else {
+      res.status(404).send("Data not found")
+    }
   })
 
 
@@ -193,36 +205,36 @@ app.post("/student/newpassword",(req,res)=>{
 
 // Moderator PIN
 
-app.post("/moderator/pin",(req,res)=>{
-  
+app.post("/moderator/pin", (req, res) => {
+
   const randomPin = Math.floor(1000 + Math.random() * 9000)
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Method:GET,POST,PUT,DELETE");
-  moderatorData.findOneAndUpdate({email:req.body.email},{$set:{pin:randomPin}}).then(()=>{
+  moderatorData.findOneAndUpdate({ email: req.body.email }, { $set: { pin: randomPin } }).then(() => {
     const mail = req.body.email
-    sendMail(step=0,mail,randomPin).then((result)=>console.log("Send Successfully",result))
-    .catch((error)=>{console.log(error)})
+    sendMail(step = 0, mail, randomPin).then((result) => console.log("Send Successfully", result))
+      .catch((error) => { console.log(error) })
     res.send()
   })
 
 })
 // Moderator New Password
 
-app.post("/moderator/newpassword",(req,res)=>{
-  moderatornew = (Math.random().toString(36).substring(2, 5)+"mod"+Math.random().toString(36).substring(2, 6))
+app.post("/moderator/newpassword", (req, res) => {
+  moderatornew = (Math.random().toString(36).substring(2, 5) + "mod" + Math.random().toString(36).substring(2, 6))
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Method:GET,POST,PUT,DELETE");
   pin = req.body.pin
-  moderatorData.findOneAndUpdate({email:req.body.email,pin:pin},{$set:{password:moderatornew}}).then((data)=>{
-    if(data !== null){
+  moderatorData.findOneAndUpdate({ email: req.body.email, pin: pin }, { $set: { password: moderatornew } }).then((data) => {
+    if (data !== null) {
       const mail = req.body.email
-      sendMail(step=1,mail,moderatornew).then((result)=>console.log(result))
-      .catch((error)=>{console.log(error)})
+      sendMail(step = 1, mail, moderatornew).then((result) => console.log(result))
+        .catch((error) => { console.log(error) })
       res.send()
-      }
-      else{
-        res.status(404).send("Data not found")
-      }
+    }
+    else {
+      res.status(404).send("Data not found")
+    }
   })
 
 
@@ -231,41 +243,87 @@ app.post("/moderator/newpassword",(req,res)=>{
 
 // Admin PIN
 
-app.post("/admin/pin",(req,res)=>{
-  
+app.post("/admin/pin", (req, res) => {
+
   const randomPin = Math.floor(1000 + Math.random() * 9000)
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Method:GET,POST,PUT,DELETE");
-  adminData.findOneAndUpdate({email:req.body.email},{$set:{pin:randomPin}}).then(()=>{
+  adminData.findOneAndUpdate({ email: req.body.email }, { $set: { pin: randomPin } }).then((data) => {
+    console.log(data)
     const mail = req.body.email
-    sendMail(step=0,mail,randomPin).then((result)=>console.log("Send Successfully",result))
-    .catch((error)=>{console.log(error)})
+    sendMail(step = 0, mail, randomPin).then((result) => console.log("Send Successfully", result))
+      .catch((error) => { console.log(error) })
     res.send()
   })
 
 })
 // Admin New Password
 
-app.post("/admin/newpassword",(req,res)=>{
-  adminnew = (Math.random().toString(36).substring(2, 5)+"adm"+Math.random().toString(36).substring(2, 6))
+app.put("/admin/newpassword", (req, res) => {
+  adminnew = (Math.random().toString(36).substring(2, 5) + "adm" + Math.random().toString(36).substring(2, 6))
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Method:GET,POST,PUT,DELETE");
   pin = req.body.pin
-  adminData.findOneAndUpdate({email:req.body.email,pin:pin},{$set:{password:adminnew}}).then((data)=>{
+  adminData.findOneAndUpdate({ email: req.body.email, pin: pin }, { $set: { password: adminnew } }).then((data) => {
     console.log(pin)
-    if(data !== null){
+    if (data !== null) {
       const mail = req.body.email
-      sendMail(step=1,mail,adminnew).then((result)=>console.log(result))
-      .catch((error)=>{console.log(error)})
+      sendMail(step = 1, mail, adminnew).then((result) => console.log(result))
+        .catch((error) => { console.log(error) })
       res.send()
-      }
-      else{
-        res.status(404).send("Data not found")
-      }
+    }
+    else {
+      res.status(404).send("Data not found")
+    }
   })
 
 
 
+})
+
+app.post('/student/register', (req, res) => {
+  const generateQR =text => {
+    try {
+      console.log( QRCode.toDataURL(text))
+    } catch (err) {
+      console.error(err)
+    }
+  }
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Method:GET,POST,PUT,DELETE");
+  register = req.body
+  let text = `Name : ${register.name} | Phone : ${register.phone} | Email : ${register.email} | Course : ${register.course} | Batch : ${register.batch} | Validity : ${register.startDate} - ${register.endDate}`
+  qr = generateQR(text)
+
+  studentData.findOneAndUpdate({ email: register.email }, {
+    $set: {
+      name: register.name,
+      phone: register.phone,
+      course:register.course,
+      batch: register.batch,
+      image: register.image,
+      qrcode: qr,
+      startDate: register.startDate,
+      endDate: register.endDate
+    }
+  }).then((data) => {
+    res.send(data)
+
+  })
+
+})
+
+app.get("/idcard/:id",(req,res)=>{
+
+
+  studentData.findOne({_id:req.params.id},{email:0,password:0,pin:0,_id:0,image:0}).then((register)=>{
+   
+
+res.send(register)
+  })
+
+
+  
 })
 
 
