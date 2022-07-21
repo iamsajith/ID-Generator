ef9d8d6b500cff46e29c807e92bee730ebbafc4b
Amalv1997 <619amalv@gmail.com>
2022-07-21 19:40:39 +0530

    add animations

--- Frontend/src/app/student/apply/apply.component.ts
+++ Frontend/src/app/student/apply/apply.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { RegistrationModel } from 'src/app/Model/student.model';
+import { StudentService } from 'src/app/student.service';
+import * as AOS from 'aos';
 
 @Component({
   selector: 'app-apply',
@@ -6,27 +9,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./apply.component.css']
 })
 export class ApplyComponent implements OnInit {
- courses = ["FSD","Data Science", "AI & ML","Software Testing"]
- batches = ["KKEM-FEB-2022","ABCD-B1-MAR-2022","ABCD-B2-MAR-2022","KKEM-MAY-2022"]
 
-  data:any = {
-    name:"",
-    email:"",
-    phone:"+91",
-    image:"",
-    course:"",
-    batch:"",
-    startDate:"",
-    endDate:"",
-  }
- 
+  courses = ["FSD", "Data Science", "AI & ML", "Software Testing"]
+  batches = ["KKEM-FEB-2022", "ABCD-B1-MAR-2022", "ABCD-B2-MAR-2022", "KKEM-MAY-2022"]
 
-  constructor() { }
+  data: any = new RegistrationModel("", "", "", "", "", "", "")
+
+
+  constructor(private _student: StudentService) { }
 
   ngOnInit(): void {
+    AOS.init();
   }
-  Register(){}
-  
-  
+  Register() {
+    this._student.studentRegister(this.data).subscribe((data) => {
+      console.log(data)
+    })
+  }
+
+
 
 }
